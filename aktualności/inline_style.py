9cc85a6b98140d547e28603fecc3760fa74b437b
dawidsowa <dawid_sowa@posteo.net>
2023-08-09 14:52:11 +0000

    9 sie 2023, 16:52

--- aktualności/inline_style.py
+++ aktualności/inline_style.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python
 
-# from json import loads as parse
-# from subprocess import run
-# from sys import argv
-
 import argparse
 
 def ${TM_FILENAME_BASE}():
+    pass
 
 
 
@@ -14,3 +11,5 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog="${TM_FILENAME_BASE}")
     parser.add_argument("file", metavar="file", type=str, nargs=1, help="file name")
     args = parser.parse_args()
+
+    ${TM_FILENAME_BASE}()
