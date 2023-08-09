747075f7257ecda96c1f412550f33aa583a7fb15
dawidsowa <dawid_sowa@posteo.net>
2023-08-09 15:04:34 +0000

    9 sie 2023, 17:04

--- aktualności/InlineStyle.py
+++ aktualności/InlineStyle.py
@@ -18,7 +18,14 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog="InlineStyle")
     parser.add_argument("html_file", metavar="html_file", type=Path, help="file name")
     parser.add_argument("css_file", metavar="css_file", type=Path, help="file name")
-    parser.add_argument("css_file", metavar="css_file", type=Path, help="file name")
+    parser.add_argument(
+        "output",
+        metavar="output_file",
+        nargs="?",
+        default=None,
+        type=Path,
+        help="file name",
+    )
     args = parser.parse_args()
 
     InlineStyle(args.html_file, args.css_file)
