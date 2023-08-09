4ae4b7f3dd9b43c5aab4bbaf34ab8f6639cd7b43
dawidsowa <dawid_sowa@posteo.net>
2023-08-09 15:25:08 +0000

    9 sie 2023, 17:25

--- aktualności/InlineStyle.py
+++ aktualności/InlineStyle.py
@@ -23,7 +23,7 @@ def InlineStyle(
     joined = f"<style>\n{css}\n</style>\n{html}"
 
     inliner = css_inline.CSSInliner(extra_css=css)
-    inlined = inliner.inline(joined)
+    inlined = inliner.inline(joined)[1:-1]
 
     if output_file:
         output_file.write_text(inlined)
